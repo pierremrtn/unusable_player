3434f1fe0e31769d3642292d7adc9d0294752750
pmtothemoon <pmtothemoon@gmail.com>
2021-09-08 16:53:35 +0200

    icons

--- lib/pages/ui_kit/ui_kit.dart
+++ lib/pages/ui_kit/ui_kit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:neat/neat.dart';
 import 'package:unusable_player/theme/dims.dart';
+import 'package:unusable_player/theme/unusable_player_icons.dart';
 
 class UIKit extends StatelessWidget {
   const UIKit();
@@ -11,20 +12,59 @@ class UIKit extends StatelessWidget {
       body: SingleChildScrollView(
         padding: EdgeInsets.all(Dims.space[1]),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Align(child: Headline1("UI Kit")),
-            Headline1("Healdine 1"),
-            Headline2("Headline 2"),
-            Headline3("Headline 3"),
-            Headline4("Headline 4"),
-            Headline5("Headline 5"),
-            Subtitle1("Subtitle 1"),
-            Subtitle2("Subtitle 2"),
-            BodyText1("BodyText 1"),
+            Divider(height: Dims.space[0]),
+            ...texts,
+            Divider(height: Dims.space[0]),
+            icons,
           ],
         ),
       ),
     );
   }
+
+  get texts => [
+        Headline1("Healdine 1"),
+        Headline2("Headline 2"),
+        Headline3("Headline 3"),
+        Headline4("Headline 4"),
+        Headline5("Headline 5"),
+        Subtitle1("Subtitle 1"),
+        Subtitle2("Subtitle 2"),
+        BodyText1("BodyText 1"),
+      ];
+
+  get icons => Wrap(
+        children: [
+          Icon(UnusablePlayerIcons.search),
+          Icon(UnusablePlayerIcons.library_music),
+          Icon(UnusablePlayerIcons.queue_music),
+          Icon(UnusablePlayerIcons.play_circle_filled),
+          Icon(UnusablePlayerIcons.fast_backward),
+          Icon(UnusablePlayerIcons.fast_forward),
+          Icon(UnusablePlayerIcons.stop),
+          Icon(UnusablePlayerIcons.play),
+          Icon(UnusablePlayerIcons.to_end),
+          Icon(UnusablePlayerIcons.to_start),
+          Icon(UnusablePlayerIcons.headset),
+          Icon(UnusablePlayerIcons.heart_1),
+          Icon(UnusablePlayerIcons.heart_empty),
+          Icon(UnusablePlayerIcons.home),
+          Icon(UnusablePlayerIcons.note),
+          Icon(UnusablePlayerIcons.loop),
+          Icon(UnusablePlayerIcons.left),
+          Icon(UnusablePlayerIcons.share),
+          Icon(UnusablePlayerIcons.pause),
+          Icon(UnusablePlayerIcons.settings),
+          Icon(UnusablePlayerIcons.note_beamed),
+          Icon(UnusablePlayerIcons.shuffle),
+          Icon(UnusablePlayerIcons.cd),
+          Icon(UnusablePlayerIcons.globe),
+          Icon(UnusablePlayerIcons.plus_circled),
+          Icon(UnusablePlayerIcons.cancel_circled),
+          Icon(UnusablePlayerIcons.folder),
+        ],
+      );
 }
