5ba24138c4d5702442ef39792f8168d2bdf4ab06
pmtothemoon <pmtothemoon@gmail.com>
2021-09-08 16:37:12 +0200

    rename dimensions.dart

--- lib/theme/dimensions.dart
+++
@@ -1,7 +0,0 @@
-import 'package:neat/neat.dart';
-
-@neat
-class Dimensions {
-  @nt_space
-  static const List<double> space = [120, 80, 53, 32, 12];
-}
---
+++ lib/theme/dims.dart
@@ -0,0 +1,7 @@
+import 'package:neat/neat.dart';
+
+@neat
+class Dims {
+  @nt_space
+  static const List<double> space = [48, 32, 21, 12, 5];
+}
