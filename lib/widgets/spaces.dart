3573bc4931b2eb36b77f68016f08a27647fcb607
pmtothemoon <pmtothemoon@gmail.com>
2021-09-09 14:28:32 +0200

    spaces generator

---
+++ lib/widgets/spaces.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+import 'package:neat/neat.dart';
+
+import 'package:unusable_player/theme/dimensions.dart';
+
+part 'spaces.nt.dart';
+
+@nt_space
+const List<double> _spaces = [
+  AppDimensions.space1,
+  AppDimensions.space2,
+  AppDimensions.space3,
+  AppDimensions.space4,
+  AppDimensions.space5,
+];
