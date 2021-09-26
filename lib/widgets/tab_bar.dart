b04c4b2dc99c489198845b6e971c1c110fa59f08
pmtothemoon <pmtothemoon@gmail.com>
2021-09-26 16:55:39 +0200

    remove bottom border from TabBar

--- lib/widgets/tab_bar.dart
+++ lib/widgets/tab_bar.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart' as flutter show TabBar;
 
 import 'package:unusable_player/theme/theme.dart' as up;
 
-const double _kBorderSize = 1;
-const double kTabBarHeight = kToolbarHeight + _kBorderSize;
+const double kTabBarHeight = kToolbarHeight;
 
 class TabBar extends StatelessWidget with PreferredSizeWidget {
   const TabBar({
@@ -17,59 +16,45 @@ class TabBar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    final flutter.TabBar tabBar = _buildTabBar(context);
-
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            width: _kBorderSize,
-            color: up.Colors.lightGrey,
-          ),
+    return flutter.TabBar(
+      controller: controller,
+      tabs: tabs
+          .map(
+            (tab) => Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: up.Dimensions.space5,
+              ),
+              child: Text(
+                tab,
+                overflow: TextOverflow.fade,
+              ),
+            ),
+          )
+          .toList(),
+      padding: EdgeInsets.only(bottom: up.Dimensions.space4),
+      //labels
+      labelPadding: EdgeInsets.zero,
+      labelStyle: Theme.of(context)
+          .textTheme
+          .headline3!
+          .copyWith(fontWeight: FontWeight.bold),
+      labelColor: Theme.of(context).colorScheme.onSurface,
+      unselectedLabelStyle: Theme.of(context)
+          .textTheme
+          .headline3!
+          .copyWith(fontWeight: FontWeight.normal),
+      unselectedLabelColor: up.Colors.lightGrey,
+      //indicator
+      indicatorColor: Theme.of(context).colorScheme.secondary,
+      indicatorSize: TabBarIndicatorSize.label,
+      indicator: UnderlineTabIndicator(
+        borderSide: BorderSide(
+          width: Theme.of(context).textTheme.headline3!.fontSize! / 1.2,
+          color: Theme.of(context).colorScheme.secondary,
         ),
       ),
-      child: tabBar,
     );
   }
-
-  flutter.TabBar _buildTabBar(BuildContext context) => flutter.TabBar(
-        controller: controller,
-        tabs: tabs
-            .map(
-              (tab) => Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: up.Dimensions.space5,
-                ),
-                child: Text(
-                  tab,
-                  overflow: TextOverflow.fade,
-                ),
-              ),
-            )
-            .toList(),
-        padding: EdgeInsets.only(bottom: up.Dimensions.space4),
-        //labels
-        labelPadding: EdgeInsets.zero,
-        labelStyle: Theme.of(context)
-            .textTheme
-            .headline3!
-            .copyWith(fontWeight: FontWeight.bold),
-        labelColor: Theme.of(context).colorScheme.onSurface,
-        unselectedLabelStyle: Theme.of(context)
-            .textTheme
-            .headline3!
-            .copyWith(fontWeight: FontWeight.normal),
-        unselectedLabelColor: up.Colors.lightGrey,
-        //indicator
-        indicatorColor: Theme.of(context).colorScheme.secondary,
-        indicatorSize: TabBarIndicatorSize.label,
-        indicator: UnderlineTabIndicator(
-          borderSide: BorderSide(
-            width: Theme.of(context).textTheme.headline3!.fontSize! / 1.2,
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-        ),
-      );
 
   @override
   Size get preferredSize => Size.fromHeight(kTabBarHeight);
