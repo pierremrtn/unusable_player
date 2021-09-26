7ebaa7e4f755f5bac5e18a1aa3699c4dc46270d6
pmtothemoon <pmtothemoon@gmail.com>
2021-09-26 12:08:47 +0200

    remove appBar param from SliverPage

--- lib/widgets/sliver_page.dart
+++ lib/widgets/sliver_page.dart
@@ -6,26 +6,25 @@ class SliverPage extends StatelessWidget {
   const SliverPage({
     this.child,
     this.padding = const EdgeInsets.symmetric(horizontal: up.Dimensions.space2),
-    this.appBar,
     required this.slivers,
   });
 
   final Widget? child;
   final EdgeInsetsGeometry padding;
-  final SliverAppBar? appBar;
   final List<Widget> slivers;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Get.theme.colorScheme.background,
-      body: Padding(
-        padding: padding,
-        child: CustomScrollView(
-          slivers: [
-            if (appBar != null) appBar!,
-            ...slivers,
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: padding,
+          child: CustomScrollView(
+            slivers: [
+              ...slivers,
+            ],
+          ),
         ),
       ),
     );
