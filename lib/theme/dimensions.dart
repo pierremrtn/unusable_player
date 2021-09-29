042cf9ed5c33334a522c078c8154fa489f2753c9
pmtothemoon <pmtothemoon@gmail.com>
2021-09-29 14:29:13 +0200

    adjust dimensions

--- lib/theme/dimensions.dart
+++ lib/theme/dimensions.dart
@@ -1,22 +1,26 @@
-import 'package:flutter/rendering.dart';
-
 class Dimensions {
+  //spaces
   static const double space1 = 32;
   static const double space2 = 26;
   static const double space3 = 18;
   static const double space4 = 12;
   static const double space5 = 5;
 
+  //borders
   static const double borderRadius1 = 24;
   static const double borderRadius2 = 18;
   static const double borderRadius3 = 12;
   static const double borderSize = 2;
 
+  //icons
   static const double icon1 = 26;
   static const double icon2 = 16;
 
-  //TODO: image1 size
+  //images
   static const double image1 = 144;
   static const double image2 = 108;
   static const double image3 = 55;
+
+  //dividers
+  static const double divider1 = 1;
 }
