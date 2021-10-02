c2298b60f1a527ad9e16e0776cdb2e934fcbecd3
pmtothemoon <pmtothemoon@gmail.com>
2021-10-02 16:33:07 +0200

    empty app bar

--- lib/widgets/app_bars/app_bars.dart
+++ lib/widgets/app_bars/app_bars.dart
@@ -1,1 +1,2 @@
 export 'normal_app_bar.dart';
+export 'empty_app_bar.dart';
---
+++ lib/widgets/app_bars/empty_app_bar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+AppBar emptyAppBar() => AppBar(
+      backgroundColor: Get.theme.colorScheme.background,
+      toolbarHeight: 0,
+      elevation: 0,
+    );
