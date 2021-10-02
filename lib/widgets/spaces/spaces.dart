c18b2c5aefdd893abfbbd878bd8017bbd3d4ce39
pmtothemoon <pmtothemoon@gmail.com>
2021-10-02 16:38:04 +0200

    update neat

--- lib/widgets/spaces/spaces.dart
+++ lib/widgets/spaces/spaces.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/widgets.dart';
-import 'package:neat/neat.dart';
+import 'package:neat/generator.dart';
 import 'package:unusable_player/theme/theme.dart' as up;
 
 part 'spaces.nt.dart';
