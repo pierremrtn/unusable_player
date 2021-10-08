b075727547dbce2fef6e16204388e5afa47ef399
pmtothemoon <pmtothemoon@gmail.com>
2021-10-08 17:56:50 +0200

    introduce general export file

---
+++ lib/unusable_player.dart
@@ -0,0 +1,5 @@
+export 'package:unusable_player/models/models.dart';
+export 'package:unusable_player/theme/theme.dart';
+export 'package:unusable_player/services/services.dart';
+export 'package:unusable_player/pages/pages.dart';
+export 'package:unusable_player/widgets/widgets.dart';
