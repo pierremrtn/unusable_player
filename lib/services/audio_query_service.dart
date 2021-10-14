af7e936fc108c8894079ca4d03ea08401d00f45a
pmtothemoon <pmtothemoon@gmail.com>
2021-10-14 10:07:17 +0200

    request permissions to read local songs

--- lib/services/audio_query_service.dart
+++ lib/services/audio_query_service.dart
@@ -15,24 +15,28 @@ extension _ToSong on SongModel {
 }
 
 class AudioFilesLoader extends GetxController with StateMixin<List<Song>> {
-  static AudioFilesLoader get get => Get.find<AudioFilesLoader>();
-
   Future<AudioFilesLoader> init() async {
+    await _ensurePermissionGranted();
     await _queryAudioFiles();
     return this;
   }
 
   final OnAudioQuery _audioQuery = OnAudioQuery();
 
+  Future<void> _ensurePermissionGranted() async {
+    while (await _audioQuery.permissionsStatus() == false) {
+      await _audioQuery.permissionsRequest();
+    }
+  }
+
   Future<void> _queryAudioFiles() async {
     try {
-      final songsModelList = await _audioQuery.querySongs();
-      final songs = songsModelList
+      final queryResult = await _audioQuery.querySongs();
+      final songs = queryResult
           .map((model) => model.toSong())
           .where((e) => e != null)
           .map((e) => e!)
           .toList();
-
       change(songs, status: RxStatus.success());
     } catch (e) {
       change([], status: RxStatus.error(e.toString()));
