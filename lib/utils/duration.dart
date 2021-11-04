70b721cc92da008e6ded8bb92ac05e9437d9246d
Pierre2tm <pmtothemoon@gmail.com>
2021-11-04 18:52:36 +0100

    duration format now display fixed length integer

--- lib/utils/duration.dart
+++ lib/utils/duration.dart
@@ -1,11 +1,10 @@
 extension DurationFormat on Duration {
   String get display {
-    if (inDays >= 1) {
-      return "$inDays:${inHours % 60}:${inMinutes % 60}:${inSeconds % 60}";
-    } else if (inHours >= 1) {
-      return "${inHours % 60}:${inMinutes % 60}:${inSeconds % 60}";
-    } else {
-      return "$inMinutes:${inSeconds % 60}";
-    }
+    String days = inDays >= 1 ? "${inDays..toString().padLeft(2, "0")}:" : "";
+    String hours =
+        inHours >= 1 ? "${(inHours % 60).toString().padLeft(2, "0")}:" : "";
+    String minutes = "${(inMinutes % 60).toString().padLeft(2, "0")}:";
+    String seconds = (inSeconds % 60).toString().padLeft(2, "0");
+    return "$days$hours$minutes$seconds";
   }
 }
