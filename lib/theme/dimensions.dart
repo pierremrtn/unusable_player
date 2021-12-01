21e5ba4814f2faaa833ac2d3c5988214ad98bbeb
Pierre2tm <pmtothemoon@gmail.com>
2021-12-01 16:27:48 +0100

    add neat padding generation

--- lib/theme/dimensions.dart
+++ lib/theme/dimensions.dart
@@ -2,7 +2,7 @@ import 'package:neat/generator.dart';
 
 part 'dimensions.nt.dart';
 
-@Neat.generateSpace
+@Neat.generate
 class Dimensions {
   //spaces
   static const double space1 = 32;
@@ -10,6 +10,13 @@ class Dimensions {
   static const double space3 = 18;
   static const double space4 = 12;
   static const double space5 = 5;
+
+  //paddings
+  static const double padding1 = space1;
+  static const double padding2 = space2;
+  static const double padding3 = space3;
+  static const double padding4 = space4;
+  static const double padding5 = space5;
 
   //borders
   static const double borderRadius1 = 24;
--- lib/theme/dimensions.nt.dart
+++ lib/theme/dimensions.nt.dart
@@ -6,8 +6,173 @@
 part of 'dimensions.dart';
 
 // **************************************************************************
-// SpaceWidgetsGenerator
+// GlobalGenerator
 // **************************************************************************
+
+const right = 0x1000;
+const left = 0x0100;
+const top = 0x0010;
+const bottom = 0x0001;
+
+class Padding1 extends EdgeInsets {
+  const Padding1([int padding = 0])
+      : super.only(
+          left: padding & left == left ? 32 : 0,
+          right: padding & right == right ? 32 : 0,
+          top: padding & top == top ? 32 : 0,
+          bottom: padding & bottom == bottom ? 32 : 0,
+        );
+
+  const Padding1.all() : super.all(32);
+
+  const Padding1.only(
+      {bool left = false,
+      bool right = false,
+      bool top = false,
+      bool bottom = false})
+      : super.only(
+          left: left ? 32 : 0,
+          right: right ? 32 : 0,
+          top: top ? 32 : 0,
+          bottom: bottom ? 32 : 0,
+        );
+
+  const Padding1.horizontal() : super.symmetric(horizontal: 32, vertical: 0);
+
+  const Padding1.vertical() : super.symmetric(vertical: 32, horizontal: 0);
+
+  const Padding1.symmetric({bool horizontal = false, bool vertical = false})
+      : super.symmetric(
+            horizontal: horizontal ? 32 : 0, vertical: vertical ? 32 : 0);
+}
+
+class Padding2 extends EdgeInsets {
+  const Padding2([int padding = 0])
+      : super.only(
+          left: padding & left == left ? 26 : 0,
+          right: padding & right == right ? 26 : 0,
+          top: padding & top == top ? 26 : 0,
+          bottom: padding & bottom == bottom ? 26 : 0,
+        );
+
+  const Padding2.all() : super.all(26);
+
+  const Padding2.only(
+      {bool left = false,
+      bool right = false,
+      bool top = false,
+      bool bottom = false})
+      : super.only(
+          left: left ? 26 : 0,
+          right: right ? 26 : 0,
+          top: top ? 26 : 0,
+          bottom: bottom ? 26 : 0,
+        );
+
+  const Padding2.horizontal() : super.symmetric(horizontal: 26, vertical: 0);
+
+  const Padding2.vertical() : super.symmetric(vertical: 26, horizontal: 0);
+
+  const Padding2.symmetric({bool horizontal = false, bool vertical = false})
+      : super.symmetric(
+            horizontal: horizontal ? 26 : 0, vertical: vertical ? 26 : 0);
+}
+
+class Padding3 extends EdgeInsets {
+  const Padding3([int padding = 0])
+      : super.only(
+          left: padding & left == left ? 18 : 0,
+          right: padding & right == right ? 18 : 0,
+          top: padding & top == top ? 18 : 0,
+          bottom: padding & bottom == bottom ? 18 : 0,
+        );
+
+  const Padding3.all() : super.all(18);
+
+  const Padding3.only(
+      {bool left = false,
+      bool right = false,
+      bool top = false,
+      bool bottom = false})
+      : super.only(
+          left: left ? 18 : 0,
+          right: right ? 18 : 0,
+          top: top ? 18 : 0,
+          bottom: bottom ? 18 : 0,
+        );
+
+  const Padding3.horizontal() : super.symmetric(horizontal: 18, vertical: 0);
+
+  const Padding3.vertical() : super.symmetric(vertical: 18, horizontal: 0);
+
+  const Padding3.symmetric({bool horizontal = false, bool vertical = false})
+      : super.symmetric(
+            horizontal: horizontal ? 18 : 0, vertical: vertical ? 18 : 0);
+}
+
+class Padding4 extends EdgeInsets {
+  const Padding4([int padding = 0])
+      : super.only(
+          left: padding & left == left ? 12 : 0,
+          right: padding & right == right ? 12 : 0,
+          top: padding & top == top ? 12 : 0,
+          bottom: padding & bottom == bottom ? 12 : 0,
+        );
+
+  const Padding4.all() : super.all(12);
+
+  const Padding4.only(
+      {bool left = false,
+      bool right = false,
+      bool top = false,
+      bool bottom = false})
+      : super.only(
+          left: left ? 12 : 0,
+          right: right ? 12 : 0,
+          top: top ? 12 : 0,
+          bottom: bottom ? 12 : 0,
+        );
+
+  const Padding4.horizontal() : super.symmetric(horizontal: 12, vertical: 0);
+
+  const Padding4.vertical() : super.symmetric(vertical: 12, horizontal: 0);
+
+  const Padding4.symmetric({bool horizontal = false, bool vertical = false})
+      : super.symmetric(
+            horizontal: horizontal ? 12 : 0, vertical: vertical ? 12 : 0);
+}
+
+class Padding5 extends EdgeInsets {
+  const Padding5([int padding = 0])
+      : super.only(
+          left: padding & left == left ? 5 : 0,
+          right: padding & right == right ? 5 : 0,
+          top: padding & top == top ? 5 : 0,
+          bottom: padding & bottom == bottom ? 5 : 0,
+        );
+
+  const Padding5.all() : super.all(5);
+
+  const Padding5.only(
+      {bool left = false,
+      bool right = false,
+      bool top = false,
+      bool bottom = false})
+      : super.only(
+          left: left ? 5 : 0,
+          right: right ? 5 : 0,
+          top: top ? 5 : 0,
+          bottom: bottom ? 5 : 0,
+        );
+
+  const Padding5.horizontal() : super.symmetric(horizontal: 5, vertical: 0);
+
+  const Padding5.vertical() : super.symmetric(vertical: 5, horizontal: 0);
+
+  const Padding5.symmetric({bool horizontal = false, bool vertical = false})
+      : super.symmetric(
+            horizontal: horizontal ? 5 : 0, vertical: vertical ? 5 : 0);
+}
 
 class Space1 extends SizedBox {
   const Space1({Key? key})
@@ -119,259 +284,6 @@ class Space5 extends SizedBox {
   const Space5.h({Key? key})
       : super(
           height: 5,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceBorderRadius1 extends SizedBox {
-  const SpaceBorderRadius1({Key? key})
-      : super(
-          height: 24,
-          width: 24,
-          key: key,
-        );
-
-  const SpaceBorderRadius1.w({Key? key})
-      : super(
-          height: 0,
-          width: 24,
-          key: key,
-        );
-
-  const SpaceBorderRadius1.h({Key? key})
-      : super(
-          height: 24,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceBorderRadius2 extends SizedBox {
-  const SpaceBorderRadius2({Key? key})
-      : super(
-          height: 18,
-          width: 18,
-          key: key,
-        );
-
-  const SpaceBorderRadius2.w({Key? key})
-      : super(
-          height: 0,
-          width: 18,
-          key: key,
-        );
-
-  const SpaceBorderRadius2.h({Key? key})
-      : super(
-          height: 18,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceBorderRadius3 extends SizedBox {
-  const SpaceBorderRadius3({Key? key})
-      : super(
-          height: 12,
-          width: 12,
-          key: key,
-        );
-
-  const SpaceBorderRadius3.w({Key? key})
-      : super(
-          height: 0,
-          width: 12,
-          key: key,
-        );
-
-  const SpaceBorderRadius3.h({Key? key})
-      : super(
-          height: 12,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceBorderSize extends SizedBox {
-  const SpaceBorderSize({Key? key})
-      : super(
-          height: 2,
-          width: 2,
-          key: key,
-        );
-
-  const SpaceBorderSize.w({Key? key})
-      : super(
-          height: 0,
-          width: 2,
-          key: key,
-        );
-
-  const SpaceBorderSize.h({Key? key})
-      : super(
-          height: 2,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceIcon1 extends SizedBox {
-  const SpaceIcon1({Key? key})
-      : super(
-          height: 26,
-          width: 26,
-          key: key,
-        );
-
-  const SpaceIcon1.w({Key? key})
-      : super(
-          height: 0,
-          width: 26,
-          key: key,
-        );
-
-  const SpaceIcon1.h({Key? key})
-      : super(
-          height: 26,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceIcon2 extends SizedBox {
-  const SpaceIcon2({Key? key})
-      : super(
-          height: 16,
-          width: 16,
-          key: key,
-        );
-
-  const SpaceIcon2.w({Key? key})
-      : super(
-          height: 0,
-          width: 16,
-          key: key,
-        );
-
-  const SpaceIcon2.h({Key? key})
-      : super(
-          height: 16,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceImage1 extends SizedBox {
-  const SpaceImage1({Key? key})
-      : super(
-          height: 144,
-          width: 144,
-          key: key,
-        );
-
-  const SpaceImage1.w({Key? key})
-      : super(
-          height: 0,
-          width: 144,
-          key: key,
-        );
-
-  const SpaceImage1.h({Key? key})
-      : super(
-          height: 144,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceImage2 extends SizedBox {
-  const SpaceImage2({Key? key})
-      : super(
-          height: 108,
-          width: 108,
-          key: key,
-        );
-
-  const SpaceImage2.w({Key? key})
-      : super(
-          height: 0,
-          width: 108,
-          key: key,
-        );
-
-  const SpaceImage2.h({Key? key})
-      : super(
-          height: 108,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceImage3 extends SizedBox {
-  const SpaceImage3({Key? key})
-      : super(
-          height: 55,
-          width: 55,
-          key: key,
-        );
-
-  const SpaceImage3.w({Key? key})
-      : super(
-          height: 0,
-          width: 55,
-          key: key,
-        );
-
-  const SpaceImage3.h({Key? key})
-      : super(
-          height: 55,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpaceDivider1 extends SizedBox {
-  const SpaceDivider1({Key? key})
-      : super(
-          height: 1,
-          width: 1,
-          key: key,
-        );
-
-  const SpaceDivider1.w({Key? key})
-      : super(
-          height: 0,
-          width: 1,
-          key: key,
-        );
-
-  const SpaceDivider1.h({Key? key})
-      : super(
-          height: 1,
-          width: 0,
-          key: key,
-        );
-}
-
-class SpacePageMargin extends SizedBox {
-  const SpacePageMargin({Key? key})
-      : super(
-          height: 18,
-          width: 18,
-          key: key,
-        );
-
-  const SpacePageMargin.w({Key? key})
-      : super(
-          height: 0,
-          width: 18,
-          key: key,
-        );
-
-  const SpacePageMargin.h({Key? key})
-      : super(
-          height: 18,
           width: 0,
           key: key,
         );
