30ae2fb4590c8191aac1b0bed7f25577d2d8ee3c
Pierre2tm <pmtothemoon@gmail.com>
2022-04-27 17:39:38 +0200

    tied artist card

---
+++ lib/widgets/cards/tied_artist_card.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:unusable_player/unusable_player.dart' as up;
+
+final int _minAnimDurationMS = up.Feel.animationDuration.inMilliseconds;
+final int _maxAnimDurationMS = up.Feel.animationDuration.inMilliseconds * 3;
+
+class TiedArtistCard extends StatelessWidget {
+  const TiedArtistCard({
+    required this.random,
+    required this.artist,
+    this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  final Random random;
+  final VoidCallback? onTap;
+  final up.Artist artist;
+
+  int get _animDuration =>
+      ((_maxAnimDurationMS - _minAnimDurationMS) * random.nextDouble())
+          .toInt() +
+      _minAnimDurationMS;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlipInX(
+      duration: Duration(milliseconds: _animDuration),
+      child: up.Tied(
+        minAngle: -0.04,
+        maxAngle: 0.04,
+        random: random,
+        child: up.ArtistCard(
+          artist: artist,
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+}
