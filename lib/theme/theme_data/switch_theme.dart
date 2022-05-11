aa3f39be092a87c984f19d860a8348b162b64dfa
Pierre2tm <pmtothemoon@gmail.com>
2022-05-11 14:52:25 +0300

    switch button theme

--- lib/theme/theme_data/switch_theme.dart
+++ lib/theme/theme_data/switch_theme.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:unusable_player/theme/colors.dart' as up;
 
+final _trackColor = MaterialStateProperty.resolveWith((states) {
+  if (states.contains(MaterialState.selected)) {
+    return up.Colors.blue;
+  }
+  return up.Colors.mediumGrey;
+});
+
 final switchTheme = SwitchThemeData(
   thumbColor: MaterialStateProperty.all(up.Colors.black),
-  trackColor: MaterialStateProperty.all(up.Colors.mediumGrey),
+  trackColor: _trackColor,
 );
 
 final darkSwitchTheme = SwitchThemeData(
   thumbColor: MaterialStateProperty.all(up.Colors.white),
-  trackColor: MaterialStateProperty.all(up.Colors.mediumGrey),
+  trackColor: _trackColor,
 );
